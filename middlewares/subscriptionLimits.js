b098c3a021be388b35d14a0c8eec7ce049e2ca9f
Ali3905 <muhammadali30905@gmail.com>
2025-03-31 22:02:30 +0500

    allow user to add vehicles in trial period

--- middlewares/subscriptionLimits.js
+++ middlewares/subscriptionLimits.js
@@ -1,44 +1,64 @@
-const { user } = require("../models/user")
-const { subscriptionPlans } = require("../constants/constants")
-const jwt = require("jsonwebtoken")
+const { user } = require("../models/user");
+const { subscriptionPlans } = require("../constants/constants");
+const jwt = require("jsonwebtoken");
 
-
-const checkVehicleLimit = item => {
-
+const checkVehicleLimit = (item) => {
     return async (req, res, next) => {
         try {
-            const { authtoken } = req.headers
+            const { authtoken } = req.headers;
             if (!authtoken) {
                 return res.status(400).json({
                     success: false,
-                    message: "Login with correct creds"
-                })
+                    message: "Login with correct credentials",
+                });
             }
-            const data = jwt.verify(authtoken, process.env.JWT_SECRET)
-            const foundUser = await user.findById(data._id).populate("subscription")
-            console.log("bahir");
-            
-            
+
+            const data = jwt.verify(authtoken, process.env.JWT_SECRET);
+            const foundUser = await user.findById(data._id).populate("subscription");
+
+            if (!foundUser) {
+                return res.status(400).json({
+                    success: false,
+                    message: "User not found",
+                });
+            }
+
+            // Check trial period validity
+            if (
+                Date.now() <= new Date(foundUser?.trialValidTill).getTime() &&
+                foundUser.vehicles.length <= 4
+            ) {
+                return next();
+            }
+
             if (item === "vehicle") {
-                const subscriptionPlan = subscriptionPlans[foundUser.subscription.plan]
+                if (!foundUser.subscription || !foundUser.subscription.plan) {
+                    return res.status(400).json({
+                        success: false,
+                        message: "No active subscription found",
+                    });
+                }
+
+                const subscriptionPlan = subscriptionPlans[foundUser.subscription.plan];
+
                 if (foundUser.vehicles.length >= subscriptionPlan.noOfVehicles) {
                     return res.status(400).json({
                         success: false,
-                        message: "You have reached your vehicle limit"
-                    })
+                        message: "You have reached your vehicle limit",
+                    });
                 }
             }
 
-            next()
+            next();
         } catch (error) {
             return res.status(500).json({
                 success: false,
-                message: error.message
-            })
+                message: error.message,
+            });
         }
-    }
-}
+    };
+};
 
 module.exports = {
-    checkVehicleLimit
-}
+    checkVehicleLimit,
+};
