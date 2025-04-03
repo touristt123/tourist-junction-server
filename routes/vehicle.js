cc90688ec423ef80b752448ce31b9377b138f93e
Ali3905 <muhammadali30905@gmail.com>
2025-04-03 12:17:05 +0500

    fix the typo in vehicle document route

--- routes/vehicle.js
+++ routes/vehicle.js
@@ -14,7 +14,7 @@ router.get("/all/photos",handleGetUserByAuthToken, handleGetAllVehiclesImages)
 router.get("/id/:vehicleId",handleGetUserByAuthToken, handleGetVehicleById)
 router.delete("/",handleGetUserByAuthToken, handleDeleteVehicle),
 router.patch("/",handleGetUserByAuthToken, upload.fields([{ name: "photos", maxCount: 5 }]), handleUpdateVehicle)
-router.patch("/addDocuments",handleGetUserByAuthToken, upload.fields([{ name: "RC", maxCount: 1 }, { name: "permit", maxCount: 1 }, { name: "fitness", maxCount: 1 }, { name: "tax", maxCount: 1 }, { name: "Insurance", maxCount: 1 } ,{ name: "PUC", maxCount: 1 }]), handleAddDocuments)
+router.patch("/addDocuments",handleGetUserByAuthToken, upload.fields([{ name: "RC", maxCount: 1 }, { name: "Permit", maxCount: 1 }, { name: "Fitness", maxCount: 1 }, { name: "Tax", maxCount: 1 }, { name: "Insurance", maxCount: 1 } ,{ name: "PUC", maxCount: 1 }]), handleAddDocuments)
 router.delete("/deleteDocuments",handleGetUserByAuthToken, handleDeleteDocuments)
 router.patch("/truck",handleGetUserByAuthToken, upload.fields([{ name: "photos", maxCount: 5 }]), handleUpdateTruck)
 router.patch("/addToFavourite", handleGetUserByAuthToken, handleAuthorizeUserByRole(["CUSTOMER"]), handleAddVehicleToFavourite)
